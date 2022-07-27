bd8142dbf58405db6627936ed5120c03575317b5
Mahmoud-Alaishat <54620274+Mahmoud-Alaishat@users.noreply.github.com>
2022-07-27 15:01:51 +0300

    Add project files.

---
+++ project.core/Data/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace project.core.Data
+{
+    public class User : IdentityUser
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String ProfilePath { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+    }
+}
