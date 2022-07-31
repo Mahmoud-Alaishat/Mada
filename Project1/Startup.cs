3d02d4322ee7f7183d49ae5d22308a2e523a1698
Mahmoud-Alaishat <54620274+Mahmoud-Alaishat@users.noreply.github.com>
2022-07-31 15:16:49 +0300

    StartupUp V2.1

--- Project1/Startup.cs
+++ Project1/Startup.cs
@@ -35,8 +35,7 @@ namespace Project1
             services.AddScoped<IDBContext, DbContext>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IUserService, UserService>();
-            services.AddScoped<ICommentRepository,CommentRepository>();
-            services.AddScoped<ICommentService, CommentService>();
+            
             services.AddControllersWithViews();
            
             // In production, the Angular files will be served from this directory
