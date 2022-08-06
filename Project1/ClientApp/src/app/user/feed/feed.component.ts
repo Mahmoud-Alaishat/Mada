1fd755a78c2eb45c62be740641b2e34274d5d492
Mahmoud-Alaishat <54620274+Mahmoud-Alaishat@users.noreply.github.com>
2022-08-06 17:53:07 +0300

    Feed V1

--- Project1/ClientApp/src/app/user/feed/feed.component.ts
+++ Project1/ClientApp/src/app/user/feed/feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-feed',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
+  result: object;
+  email: string;
+  role: string;
+  IsAdmin = false;
 
-  constructor() { }
+  constructor(private router: Router, private jwtHelper: JwtHelperService) { }
 
   ngOnInit() {
   }
 
+  isUserAuthenticated = (): boolean => {
+    const token = localStorage.getItem("token");
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
+      this.result = this.jwtHelper.decodeToken(token);
+      this.email = this.result["email"];
+      this.role = this.result["role"];
+      return true;
+    }
+    return false;
+  }
+  isAdmin = () => {
+    if (this.role == "Admin") {
+      this.IsAdmin = true;
+      return this.IsAdmin;
+    }
+    this.IsAdmin = false;
+    return this.IsAdmin;
+  }
+  
 }
