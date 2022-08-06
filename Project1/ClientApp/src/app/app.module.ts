b9c9def8633899215e677945ef166921ee0e561e
Mahmoud-Alaishat <54620274+Mahmoud-Alaishat@users.noreply.github.com>
2022-08-06 10:13:48 +0300

    Home V1.10

--- Project1/ClientApp/src/app/app.module.ts
+++ Project1/ClientApp/src/app/app.module.ts
@@ -17,7 +17,6 @@ import { AuthGuard } from './guards/auth.guard';
 import { UserModule } from './user/user.module';
 import { AdminModule } from './admin/admin.module';
 import { RegisterComponent } from './register/register.component';
-import { MatDialogModule } from '@angular/material/dialog';
 
 
 
