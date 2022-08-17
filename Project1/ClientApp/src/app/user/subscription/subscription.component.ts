853bca76fcd2c0a840ca5d521eb89d18f9f0c246
May Hammad <mayhammad200013@gmail.com>
2022-08-17 13:01:00 +0300

    Subsciptin s2

--- Project1/ClientApp/src/app/user/subscription/subscription.component.ts
+++ Project1/ClientApp/src/app/user/subscription/subscription.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-subscription',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscription.component.css']
 })
 export class SubscriptionComponent implements OnInit {
-
-  constructor() { }
+  public subscription: Subscription[];
+  isAuthenticate: boolean = false;
+  isAdmin: boolean = false;
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService, private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.isAuthenticate = this.auth.isUserAuthenticated();
+    this.isAdmin = this.auth.isAdmin();
+    this.http.get<Subscription[]>("https://localhost:44328/api/User/GetAllSubscriptions" , {
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    }).subscribe({
+      next: (response: Subscription[]) => {
+        this.subscription = response;
+      },
+      error: (err: HttpErrorResponse) => console.log("no data")
+    })
+  }
   }
 
+
+
+interface Subscription {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  feature: string;
+  limitPost: number;
 }
