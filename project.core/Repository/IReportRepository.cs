58b96b2dd9b37fb2e9b985b570dc03ca13bdbbd5
May Hammad <mayhammad200013@gmail.com>
2022-08-17 15:56:44 +0300

    Reports V1

---
+++ project.core/Repository/IReportRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using project.core.Data;
+
+namespace project.core.Repository
+{
+    public interface IReportRepository
+    {
+        public List<Reports> CRUDOP(Reports report, string operation);
+    }
+}
---
+++ project.infra/Repository/ReportRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Dapper;
+using project.core.Data;
+using project.core.Domain;
+using project.core.Repository;
+
+namespace project.infra.Repository
+{
+    public class ReportRepository : IReportRepository
+    {
+
+        private readonly IDBContext context;
+        public ReportRepository(IDBContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Reports> CRUDOP(Reports report, string operation)
+        {
+            var parameter = new DynamicParameters();
+            List<Reports> re = new List<Reports>();
+            parameter.Add("idofRepoet", report.Id, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            parameter.Add("idofPost", report, dbType: DbType.Int32, direction: ParameterDirection.Input);
+            parameter.Add("idofStatus", report, dbType: DbType.Int32, direction: ParameterDirection.Input);
+
+            parameter.Add("operation", operation, dbType: DbType.String, direction: ParameterDirection.Input);
+            if (operation == "read" )
+            {
+                var result = context.dbConnection.Query<Reports>("Report_package_api.CRUDOP", parameter, commandType: CommandType.StoredProcedure);
+                return result.ToList();
+            }
+            else
+            {
+                context.dbConnection.ExecuteAsync("Report_package_api.CRUDOP", parameter, commandType: CommandType.StoredProcedure);
+                return re;
+            }
+        }
+    }
+}
