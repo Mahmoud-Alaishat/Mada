193a500dde129f9207dab892515ec5d326095593
MutasemShboul <mutasemshboul14@gmail.com>
2022-08-31 21:14:03 +0300

    Dashboard V1.1

--- Project1/ClientApp/src/app/admin/dashboard/dashboard.component.ts
+++ Project1/ClientApp/src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  dashboardcounter: DashboardCounter={
+      countLike: 0,
+      countComment: 0,
+      countAd: 0,
+      countPost: 0,
+      images: 0,
+      videos: 0,
+      numbofWords: 0,
+      countUser: 0
   }
 
+  revenuedetails: RevenueDetails[];
+  totalAllRevenue: number;
+  constructor(private http: HttpClient, private router: Router, private jwtHelper: JwtHelperService, private auth: AuthService) { }
+
+  ngOnInit() {
+    this.http.get<DashboardCounter>("https://localhost:44328/api/Admin/DashboardCounters/", {
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    }).subscribe({
+      next: (response: DashboardCounter) => {
+        this.dashboardcounter = response;
+
+      },
+      error: (err: HttpErrorResponse) => console.log("no data")
+    })
+
+    this.http.get<RevenueDetails[]>("https://localhost:44328/api/Admin/RevenueDetails/", {
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    }).subscribe({
+      next: (response: RevenueDetails[]) => {
+        this.revenuedetails = response;
+        this.totalAllRevenue = this.revenuedetails[0].totalRevenue + this.revenuedetails[1].totalRevenue;
+
+      },
+      error: (err: HttpErrorResponse) => console.log("no data")
+    })
+
+
+
+  }
+
+}
+
+interface DashboardCounter {
+  countLike: number;
+  countComment: number;
+  countAd: number;
+  countPost: number;
+  images: number;
+  videos: number;
+  numbofWords: number;
+  countUser: number;
+}
+
+interface RevenueDetails {
+  service: string;
+  totalRevenue: number;
+  numberOfSubscribers: number;
+  numberOfAllSubscribers: number;
 }
