95fcff6526b540700203749751bf52e5a25febe6
Mahmoud-Alaishat <54620274+Mahmoud-Alaishat@users.noreply.github.com>
2022-09-01 09:31:07 +0300

    Dashboard V1.2

--- Project1/ClientApp/src/app/admin/dashboard/dashboard.component.ts
+++ Project1/ClientApp/src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  userData: UserInfo = { firstName: '', lastName: '', profilePath: '', address: '', coverPath: '', bio: '', relationship: '' };
+  isAuthenticate: boolean = false;
+  isAdmin: boolean = false;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router, private jwtHelper: JwtHelperService, private auth: AuthService) { }
 
   ngOnInit() {
+    (function (window, document, undefined) {
+      'use strict';
+      if (!('localStorage' in window)) return;
+      var nightMode = localStorage.getItem('gmtNightMode');
+      if (nightMode) {
+        document.documentElement.className += ' night-mode';
+      }
+    })(window, document);
+
+    (function (window, document, undefined) {
+
+      'use strict';
+
+      // Feature test
+      if (!('localStorage' in window)) return;
+
+      // Get our newly insert toggle
+      var nightMode = document.querySelector('#night-mode');
+      if (!nightMode) return;
+
+      // When clicked, toggle night mode on or off
+      nightMode.addEventListener('click', function (event) {
+        event.preventDefault();
+        document.documentElement.classList.toggle('dark');
+        if (document.documentElement.classList.contains('dark')) {
+          localStorage.setItem('gmtNightMode', 'true');
+          return;
+        }
+        localStorage.removeItem('gmtNightMode');
+      }, false);
+
+    })(window, document);
+    this.isAuthenticate = this.auth.isUserAuthenticated();
+    this.isAdmin = this.auth.isAdmin();
+    this.http.get<UserInfo>("https://localhost:44328/api/User/GetUserInfo/" + this.auth.Id, {
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    }).subscribe({
+      next: (response: UserInfo) => {
+        this.userData = response;
+      },
+      error: (err: HttpErrorResponse) => console.log("no data")
+    })
+  }
+  logOut = () => {
+    localStorage.removeItem("token");
+    this.router.navigate(["/"]);
   }
 
+}
+
+interface UserInfo {
+  firstName: string;
+  lastName: string;
+  profilePath: string;
+  coverPath: string;
+  address: string;
+  relationship: string;
+  bio: string;
 }
