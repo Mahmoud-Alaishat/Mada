750c10c2ed84a4839f685b8f5b999d98b1cfafc9
May Hammad <mayhammad200013@gmail.com>
2022-09-04 15:25:02 +0300

    About5

--- Project1/ClientApp/src/app/admin/about-us-design/about-us-design.component.ts
+++ Project1/ClientApp/src/app/admin/about-us-design/about-us-design.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpEventType, HttpHeaders } from '@angular/common/http';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthService } from '../../auth.service';
@@ -14,6 +15,22 @@ export class AboutUsDesignComponent implements OnInit {
   isAuthenticate: boolean = false;
   isAdmin: boolean = false;
   userData: UserInfo = { firstName: '', lastName: '', profilePath: '', address: '', coverPath: '', bio: '', relationship: '' };
+  aboutDesign: Design = {
+    id: '',
+    slideImage1: '',
+    slideImage2: '',
+    slideImage3: '',
+    subText1: '',
+    subText2: '',
+    subText3: '',
+    mainText1: '',
+    mainText2: '',
+    mainText3: '',
+    userId: ''
+  }
+  AboutDesignForm: FormGroup;
+  @Output() public onUploadFinished = new EventEmitter();
+  aboutImage: any;
 
   constructor(private http: HttpClient, private router: Router, private jwtHelper: JwtHelperService, private auth: AuthService) { }
 
@@ -62,13 +79,86 @@ export class AboutUsDesignComponent implements OnInit {
       },
       error: (err: HttpErrorResponse) => console.log("no data")
     })
+    this.http.get<Design>("https://localhost:44328/api/Admin/GetDesignById/AboutUs", {
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    }).subscribe({
+      next: (response: Design) => {
+        this.aboutDesign = response
+      },
+      error: (err: HttpErrorResponse) => console.log("no data")
+    })
 
   }
   logOut = () => {
     localStorage.removeItem("token");
     this.router.navigate(["/"]);
   }
+  uploadAboutImg = (files) => {
+    if (files.length === 0) {
+      return;
+    }
+    let fileToUpload = <File>files[0];
+    const formData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
 
+    this.http.post('https://localhost:44328/api/Admin/UploadAboutImg', formData, { reportProgress: true, observe: 'events' })
+      .subscribe({
+        next: (event) => {
+
+
+          if (event.type === HttpEventType.Response) {
+            this.onUploadFinished.emit(event.body);
+            this.aboutImage = event.body['aboutPath'];
+          }
+        },
+        error: (err: HttpErrorResponse) => console.log(err)
+      });
+
+  }
+  UpdateAboutUs() {
+    let maintext = (<HTMLInputElement>document.getElementById('maintext')).value;
+    let subtext = (<HTMLInputElement>document.getElementById('subtext')).value;
+
+    let maintext1 = (<HTMLInputElement>document.getElementById('maintext1')).value;
+    let subtext1 = (<HTMLInputElement>document.getElementById('subtext1')).value;
+
+    let maintext2 = (<HTMLInputElement>document.getElementById('maintext2')).value;
+    let subtext2 = (<HTMLInputElement>document.getElementById('subtext2')).value;
+
+    if (maintext != "") {
+      this.aboutDesign.mainText1 = (<HTMLInputElement>document.getElementById('maintext')).value;
+    }
+    if (subtext != "") {
+      this.aboutDesign.subText1 = (<HTMLInputElement>document.getElementById('subtext')).value;
+    }
+
+    if (maintext1 != "") {
+      this.aboutDesign.mainText2 = (<HTMLInputElement>document.getElementById('maintext1')).value;
+    }
+    if (subtext1 != "") {
+      this.aboutDesign.subText2 = (<HTMLInputElement>document.getElementById('subtext1')).value;
+    }
+
+    if (maintext2 != "") {
+      this.aboutDesign.mainText3 = (<HTMLInputElement>document.getElementById('maintext2')).value;
+    }
+    if (subtext2 != "") {
+      this.aboutDesign.subText3 = (<HTMLInputElement>document.getElementById('subtext2')).value;
+    }
+
+    if (this.aboutImage != null) {
+      this.aboutDesign.slideImage1 = this.aboutImage;
+    }
+    this.http.post("https://localhost:44328/api/Admin/UpdateDesign/", this.aboutDesign, {
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    }).subscribe({
+      next: () => {
+        window.location.reload();
+      },
+      error: (err: HttpErrorResponse) => console.log("no data")
+    })
+
+  }
 }
 
 interface UserInfo {
@@ -79,4 +169,18 @@ interface UserInfo {
   address: string;
   relationship: string;
   bio: string;
+}
+
+interface Design {
+  id: string;
+  slideImage1: string;
+  slideImage2: string;
+  slideImage3: string;
+  subText1: string;
+  subText2: string;
+  subText3: string;
+  mainText1: string;
+  mainText2: string;
+  mainText3: string;
+  userId: string;
 }
